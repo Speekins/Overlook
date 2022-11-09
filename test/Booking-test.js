6d4ad42ada9c4a3c776cad61a9f1207d73b336f0
Spencer Haka <spencer.haka@gmail.com>
2022-11-09 13:02:01 -0700

    Write booking class

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -1,3 +1,27 @@
 import chai from 'chai';
 import Booking from '../src/classes/booking'
 const expect = chai.expect;
+
+describe('booking', () => {
+  let booking
+
+  beforeEach(() => {
+    booking = new Booking(bookingData)
+  })
+
+  it.skip('should have an id', () => {
+
+  })
+
+  it.skip('should have a user id', () => {
+
+  })
+
+  it.skip('should have a date', () => {
+
+  })
+
+  it.skip('should have a room id', () => {
+
+  })
+})
